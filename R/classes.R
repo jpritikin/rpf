8f9ea5038cb389404410746463698b0a1253720d
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2012-12-23 17:59:27 -0500

    Make rpf.sample work in the multidimensional case

--- R/classes.R
+++ R/classes.R
@@ -45,6 +45,22 @@ setClass("rpf.base",
                         dimensions="numeric",
                         "VIRTUAL"))
 
+##' The base class for 1 dimensional response probability functions.
+##' @name Class rpf.1dim
+##' @rdname rpf.1dim-class
+##' @aliases rpf.1dim-class
+##' @export
+setClass("rpf.1dim", contains='rpf.base',
+         representation("VIRTUAL"))
+
+##' The base class for multi-dimensional response probability functions.
+##' @name Class rpf.mdim
+##' @rdname rpf.mdim-class
+##' @aliases rpf.mdim-class
+##' @export
+setClass("rpf.mdim", contains='rpf.base',
+         representation("VIRTUAL"))
+
 ##' Map an item model, item parameters, and person trait score into a
 ##' probability vector
 ##'
@@ -76,6 +92,9 @@ setGeneric("rpf.prob", function(m, param, theta) standardGeneric("rpf.prob"))
 
 ##' Map an item model, item parameters, and person trait score into a
 ##' probability vector
+##'
+##' This API is experimental. I am not sure whether it is worth having
+##' both rpf.prob and rpf.logprob.
 ##'
 ##' @param m an item model
 ##' @param param item parameters
@@ -119,6 +138,19 @@ setMethod("rpf.logprob", signature(m="rpf.base", param="numeric",
             log(rpf.prob(m, param, theta))
           })
 
+##' Turn a matrix of theta into a vector for
+##' \code{\link{rpf.logprob}}.
+##' 
+##' @name rpf.logprob wrapper3
+##' @rdname rpf.logprob.wrapper3
+##' @aliases rpf.logprob,rpf.1dim,numeric,matrix-method
+##' @docType methods
+setMethod("rpf.logprob", signature(m="rpf.1dim", param="numeric",
+                                theta="matrix"),
+          function(m, param, theta) {
+            rpf.logprob(m, param, as.numeric(theta))
+          })
+
 ##' Turn a matrix of parameters into a vector for
 ##' \code{\link{rpf.prob}}.
 ##' 
@@ -143,6 +175,19 @@ setMethod("rpf.prob", signature(m="rpf.base", param="matrix",
                                 theta="matrix"),
           function(m, param, theta) {
             rpf.prob(m, as.numeric(param), theta)
+          })
+
+##' Turn a matrix of theta into a vector for
+##' \code{\link{rpf.prob}}.
+##' 
+##' @name rpf.prob wrapper3
+##' @rdname rpf.prob.wrapper3
+##' @aliases rpf.prob,rpf.1dim,numeric,matrix-method
+##' @docType methods
+setMethod("rpf.prob", signature(m="rpf.1dim", param="numeric",
+                                theta="matrix"),
+          function(m, param, theta) {
+            rpf.prob(m, param, as.numeric(theta))
           })
 
 ##' Map an item model, item parameters, and person trait score into a
@@ -259,22 +304,6 @@ setGeneric("rpf.setLocation", function(m,param,loc) standardGeneric("rpf.setLoca
 ##' Laboratory, Stanford, CA.
 ##' 
 rpf.ogive <- 1.702
-
-##' The base class for 1 dimensional response probability functions.
-##' @name Class rpf.1dim
-##' @rdname rpf.1dim-class
-##' @aliases rpf.1dim-class
-##' @export
-setClass("rpf.1dim", contains='rpf.base',
-         representation("VIRTUAL"))
-
-##' The base class for multi-dimensional response probability functions.
-##' @name Class rpf.mdim
-##' @rdname rpf.mdim-class
-##' @aliases rpf.mdim-class
-##' @export
-setClass("rpf.mdim", contains='rpf.base',
-         representation("VIRTUAL"))
 
 ##' The base class for 1 dimensional graded response probability functions.
 ##' 
@@ -400,34 +429,73 @@ setClass("rpf.mdim.mcm", contains='rpf.mdim',
 ##' Randomly sample response patterns given a list of items
 ##'
 ##' Returns a random sample of response patterns given
-##' a list of unidimensional item models and parameters.
+##' a list of item models and parameters.
 ##'
 ##' @name rpf.sample
 ##' @usage
-##' rpf.sample(theta, items, params)
+##' rpf.sample(theta, items, params, design)
 ##' @param theta either a vector of trait abilities or
 ##' the number of abilities to draw from N(0,1)
 ##' @param items a list of item models
 ##' @param params a list of item parameters. If omitted, random item
 ##' parameters are generated for each item model.
+##' @param design assigns person abilities to item dimensions
 ##' @return Returns a matrix of response patterns
 ##' @export
 ##' @examples
+##' # 1 dimensional items
 ##' i1 <- rpf.drm()
 ##' i1.p <- rpf.rparam(i1)
 ##' i2 <- rpf.gpcm(numOutcomes=3)
 ##' i2.p <- rpf.rparam(i2)
 ##' rpf.sample(5, list(i1,i2), list(i1.p, i2.p))
+##'
+##' # multidimensional items
+##' numItems <- 4
+##' items <- vector("list", numItems)
+##' correct <- vector("list", numItems)
+##'
+##' i1 <- rpf.drm(dimensions=2)
+##' i2 <- rpf.drm(dimensions=1, multidimensional=TRUE)
+##'
+##' for (ix in 1:(numItems-1)) {
+##'   items[[ix]] <- i1
+##'   correct[[ix]] <- rpf.rparam(i1)
+##' }
+##' items[[4]] <- i2
+##' correct[[4]] <- rpf.rparam(i2)
+##' 
+##' design <- matrix(c(1, 1, 1, 1,
+##'                    2, 2, 3, NA), nrow=2, byrow=TRUE)
+##' rpf.sample(10, items, correct, design)
 ##' @seealso \code{\link{sample}}
-rpf.sample <- function(theta, items, params) {
-  if (max(vapply(items, function(i) i@dimensions, 0)) > 1) {
-    stop("Can only sample unidimensional items")
+rpf.sample <- function(theta, items, params, design) {
+  maxDim <- max(vapply(items, function(i) i@dimensions, 0))
+  if (missing(design)) {
+    if (maxDim > 1) {
+      stop("The design matrix must be provided for multidimensional item models")
+    }
+    design <- 1
   }
-  if (length(theta) == 1) {
-    theta <- rnorm(theta)
-  }
-  numPeople <- length(theta)
+  maxAbilities <- max(design, na.rm=TRUE)
+
   numItems <- length(items)
+  if (maxDim > 1 && any(dim(design) != c(maxDim,numItems))) {
+    stop(paste("The design matrix must have", maxDim, "rows and ",numItems,"columns"))
+  }
+
+  numPeople <- NA
+  if (is.numeric(theta) && length(theta) == 1) {
+    numPeople <- theta
+    theta <- array(rnorm(numPeople * maxAbilities),
+                   dim=c(numPeople, maxAbilities))
+  } else if (maxDim == 1 && is.vector(theta)) {
+    numPeople <- length(theta)
+    theta <- array(theta, dim=c(numPeople, maxAbilities))
+  } else {
+    numPeople <- dim(theta)[1]
+  }
+
   if (missing(params)) {
     params <- lapply(items, rpf.rparam)
   }
@@ -437,7 +505,15 @@ rpf.sample <- function(theta, items, params) {
   for (ix in 1:numItems) {
     i <- items[[ix]]
     param <- params[[ix]]
-    P <- rpf.prob(i, param, theta)
+    P <- NA
+    if (maxDim==1) {
+      P <- rpf.prob(i, param, theta)
+    } else {
+      cols <- design[,ix]
+      cols <- cols[!is.na(cols)]
+      i.theta <- as.matrix(theta[,cols])
+      P <- rpf.prob(i, param, i.theta)
+    }
     ret[,ix] <- apply(P, c(1), sample, x=1:i@numOutcomes, size=1, replace=F)
   }
   return(ret)
