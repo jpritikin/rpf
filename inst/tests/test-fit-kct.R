ff252c00b39b78a2078e6ed5aec34ad86ffbb5a8
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2013-06-06 11:49:14 +0530

    Slight reorg to hoist data out of test

--- inst/tests/test-fit-kct.R
+++ inst/tests/test-fit-kct.R
@@ -1,22 +1,24 @@
 options(error = utils::recover)
 library(testthat)
 library(rpf)
+data(kct)
 
 context("outfit/infit")
 
-test_that("kct", {
-  data(kct)
+responses <- kct.people[,paste0("V",2:19)]
+rownames(responses) <- kct.people$NAME
+responses <- responses[1:34,4:17]
+data <- sapply(responses, unclass) - 1
 
+test_that("kct", {
   scores <- kct.people$MEASURE
   params <- as.data.frame(cbind(1, kct.items$MEASURE, 0))
   rownames(params) <- kct.items$NAME
   items<-list()
   items[1:18] <- rpf.drm()
-  responses <- kct.people[,paste0("V",2:19)]
-  rownames(responses) <- kct.people$NAME
 
   fit <- rpf.1dim.fit(items[1:14], cbind(params[4:17,],1),
-                      responses[1:34,4:17], scores[1:34], 2)
+                      responses, scores[1:34], 2)
 
   expect_equal(fit$infit, kct.items$IN.MSQ[4:17], tolerance=.002)
   expect_equal(fit$infit.z, kct.items$IN.ZSTD[4:17], tolerance=.01)
