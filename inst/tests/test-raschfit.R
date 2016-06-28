ddcbc3bfc8bf1869f44cb35f9bf5e19cf19d6943
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2016-06-28 10:24:16 +0530

    No longer check Rasch fit statistics against mirt

--- inst/tests/test-raschfit.R
+++ inst/tests/test-raschfit.R
@@ -74,29 +74,3 @@ test_that("sf", {
   expect_equal(fit$outfit, sfpf$OUT.MSQ[-2], tolerance=.05)
   expect_equal(fit$outfit.z, sfpf$OUT.ZSTD[-2], tolerance=.075)
 })
-
-test_that("mirt", {
-  require(mirt)
-  set.seed(1)
-  a <- matrix(rlnorm(20, meanlog=0, sdlog = .1),ncol=1)
-  d <- matrix(rnorm(20),ncol=1)
-  data <- simdata(a,d, 1000, rep('dich', 20))
-  data <- data[-c(446, 455, 538, 616, 630, 687, 882, 894, 923),]  # exclude min or max responses
-  raschfit <- mirt(data, 1, itemtype='Rasch', D=1, verbose=FALSE)
-  # coef(raschfit)  # item parameters
-  mirt.fit <- itemfit(raschfit, infit=TRUE)
-  scores.full <- fscores(raschfit, full.scores=TRUE)
-
-  spec <- list()
-  spec[1:20] <- list(rpf.drm())
-  params <- simplify2array(coef(raschfit)[1:20])[1,,]
-  params[c('g', 'u'),] <- logit(params[c('g','u'),])
-  scores <- scores.full[,'F1']
-  data.f <- as.data.frame(lapply(as.data.frame(data), ordered))
-  fit <- rpf.1dim.fit(spec, params, data.f, scores, 2, wh.exact=TRUE)
-
-  expect_equal(mirt.fit$infit, fit$infit, tolerance=10^-4)
-  expect_equal(mirt.fit$z.infit, fit$infit.z, tolerance=10^-3)
-  expect_equal(mirt.fit$outfit, fit$outfit, tolerance=10^-4)
-  expect_equal(mirt.fit$z.outfit, fit$outfit.z, tolerance=10^-3)
-})
